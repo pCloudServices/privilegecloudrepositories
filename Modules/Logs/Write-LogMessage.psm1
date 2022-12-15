93609c6783a4e13be70c1709a087dd5d2f674d7b
Ethan Sarikas <83921121+Ethlas@users.noreply.github.com>
2022-12-15 12:56:15 +0100

    Added a module called write-logmessage.psm1

---
+++ Modules/Logs/Write-LogMessage.psm1
@@ -0,0 +1,105 @@
+Function Write-LogMessage {
+    <# 
+.SYNOPSIS 
+	Method to log a message on screen and in a log file
+
+.DESCRIPTION
+	Logging The input Message to the Screen and the Log File. 
+	The Message Type is presented in colours on the screen based on the type
+
+.PARAMETER LogFile
+	The Log File to write to. By default using the LOG_FILE_PATH
+.PARAMETER MSG
+	The message to log
+.PARAMETER Header
+	Adding a header line before the message
+.PARAMETER SubHeader
+	Adding a Sub header line before the message
+.PARAMETER Footer
+	Adding a footer line after the message
+.PARAMETER Type
+	The type of the message to log (Info, Warning, Error, Debug)
+#>
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [AllowEmptyString()]
+        [String]$MSG,
+        [Parameter(Mandatory = $false)]
+        [Switch]$Header,
+        [Parameter(Mandatory = $false)]
+        [Switch]$Early,
+        [Parameter(Mandatory = $false)]
+        [Switch]$SubHeader,
+        [Parameter(Mandatory = $false)]
+        [Switch]$Footer,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Info", "Warning", "Error", "Debug", "Verbose", "Success", "LogOnly")]
+        [String]$type = "Info",
+        [Parameter(Mandatory = $false)]
+        [String]$LogFile = $LOG_FILE_PATH
+    )
+    Try {
+        If ($Header) {
+            "=======================================" | Out-File -Append -FilePath $LogFile 
+            Write-Host "=======================================" -ForegroundColor Magenta
+        }
+        ElseIf ($SubHeader) { 
+            "------------------------------------" | Out-File -Append -FilePath $LogFile 
+            Write-Host "------------------------------------" -ForegroundColor Magenta
+        }
+		
+        $msgToWrite = "[$(Get-Date -Format "yyyy-MM-dd hh:mm:ss")]`t"
+        $writeToFile = $true
+        # Replace empty message with 'N/A'
+        if ([string]::IsNullOrEmpty($Msg)) { $Msg = "N/A" }
+		
+        # Mask Passwords
+        if ($Msg -match '((?:password|credentials|secret)\s{0,}["\:=]{1,}\s{0,}["]{0,})(?=([\w`~!@#$%^&*()-_\=\+\\\/|;:\.,\[\]{}]+))') {
+            $Msg = $Msg.Replace($Matches[2], "****")
+        }
+        # Check the message type
+        switch ($type) {
+            { ($_ -eq "Info") -or ($_ -eq "LogOnly") } { 
+                If ($_ -eq "Info") {
+                    Write-Host $MSG.ToString() -ForegroundColor $(If ($Header -or $SubHeader) { "magenta" } Elseif ($Early) { "DarkGray" } Else { "White" })
+                }
+                $msgToWrite += "[INFO]`t$Msg"
+            }
+            "Success" { 
+                Write-Host $MSG.ToString() -ForegroundColor Green
+                $msgToWrite += "[SUCCESS]`t$Msg"
+            }
+            "Warning" {
+                Write-Host $MSG.ToString() -ForegroundColor Yellow
+                $msgToWrite += "[WARNING]`t$Msg"
+            }
+            "Error" {
+                Write-Host $MSG.ToString() -ForegroundColor Red
+                $msgToWrite += "[ERROR]`t$Msg"
+            }
+            "Debug" { 
+                if ($InDebug -or $InVerbose) {
+                    Write-Debug $MSG
+                    $msgToWrite += "[DEBUG]`t$Msg"
+                }
+                else { $writeToFile = $False }
+            }
+            "Verbose" { 
+                if ($InVerbose) {
+                    Write-Verbose -Msg $MSG
+                    $msgToWrite += "[VERBOSE]`t$Msg"
+                }
+                else { $writeToFile = $False }
+            }
+        }
+
+        If ($writeToFile) { $msgToWrite | Out-File -Append -FilePath $LogFile }
+        If ($Footer) { 
+            "=======================================" | Out-File -Append -FilePath $LogFile 
+            Write-Host "=======================================" -ForegroundColor Magenta
+        }
+    }
+    catch {
+        Throw $(New-Object System.Exception ("Cannot write message"), $_.Exception)
+    }
+}
